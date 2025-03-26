a0884eed360ed6a291259d35d76b46d08088daa6
Anusha P Hullathi <anusha230902@gmail.com>
2025-03-26 12:54:34 +0530

    model trainer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,15 +1,17 @@
-#it contains the code which is used to read the file
 import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass #used to create class variables
+from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -54,6 +56,8 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-   
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
 
 
